04219d59a3e57569d46359229e477293c9b0db9e
Judith Prays <judithprays@gmail.com>
2019-02-05 10:23:21 -0800

    Shop menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){ 
+    $(".shop-menu").click(function() {
+        event.preventDefault();
+            $(".nav_expanded").toggleClass('hide', 1000, "easeOutSine");
+            $(".shop-menu").toggleClass('active');
+
+        });
+
 });
 
